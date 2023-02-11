e62fdc41870b831dd5a4793649571d5a7ff5a480
Cihat Solak <cihatsolak@hotmail.com>
2023-02-11 15:30:08 +0300

    create consumer and consume messages

---
+++ SimplePublisherConsumerApp/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿//Bağlantı oluşturma
+IConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    Port = 5672,
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Kuyruk oluşturma
+channel.QueueDeclare(
+    queue: "test-queue",
+    exclusive: false //true: sadece bağlantı kurulabilir. false: birden fazla bağlantı kurulabilir.
+    );
+
+//Kuyruktan mesaj okuma
+EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+//Kanal'dan mesaj tüketme ayarları
+channel.BasicConsume(
+    queue: "test-queue",
+    autoAck: false,
+    consumer: eventingBasicConsumer
+    );
+
+eventingBasicConsumer.Received += (sender, args) =>
+{
+    string message = Encoding.UTF8.GetString(args.Body.Span);
+    Console.WriteLine("Message: {0}", message);
+};
+
+Console.ReadKey();
