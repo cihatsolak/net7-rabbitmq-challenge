ada5ba1399504333a1350c17c3eb358a475965ff
Cihat Solak <cihatsolak@hotmail.com>
2023-02-13 23:00:56 +0300

    add direct exchange publisher

---
+++ ExchangeTypes/RabbitMQ.ExchangeTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿//Bağlantı oluşturma
+IConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    Port = 5672,
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Exchange Oluşturma
+channel.ExchangeDeclare(
+    exchange: "direct-exchange-example",
+    type: ExchangeType.Direct,
+    durable: true,
+    autoDelete: false
+    );
+
+channel.BasicPublish(
+    exchange: "direct-exchange-example",
+    routingKey: "bank",
+    body: Encoding.UTF8.GetBytes("Hello")
+    );
+
+Console.ReadKey();
