06b7f70b55798237e245a5038d82c90c27489136
Cihat Solak <cihatsolak@hotmail.com>
2023-02-18 22:48:31 +0300

    add topic exchange consumer-publisher

---
+++ ExchangeTypes/RabbitMQ.ExchangeTypes.Consumer/TopicExchange.cs
@@ -0,0 +1,49 @@
+﻿namespace RabbitMQ.ExchangeTypes.Consumer
+{
+    internal static class TopicExchange
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "fanout-exchange-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            channel.QueueBind(
+                queue: "fanout-exchange-queue",
+                exchange: "topic-exchange",
+                routingKey: "*.Yiyecek.*"
+                );
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: "fanout-exchange-queue",
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                string message = Encoding.UTF8.GetString(args.Body.Span);
+                Console.WriteLine("Message: {0}", message);
+            };
+
+        }
+    }
+}
---
+++ ExchangeTypes/RabbitMQ.ExchangeTypes.Publisher/TopicExchange.cs
@@ -0,0 +1,36 @@
+﻿namespace RabbitMQ.ExchangeTypes.Publisher
+{
+    internal static class TopicExchange
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                exchange: "topic-exchange",
+                type: ExchangeType.Topic,
+                durable: true,
+                autoDelete: false
+                );
+
+            channel.BasicPublish(
+                exchange: "topic-exchange",
+                routingKey: "Saglik.Yiyecek.*",
+                body: Encoding.UTF8.GetBytes("Hello Fanout!")
+                );
+
+            Console.ReadKey();
+        }
+    }
+}
