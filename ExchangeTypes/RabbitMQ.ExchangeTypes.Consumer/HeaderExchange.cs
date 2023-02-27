a28a753e4720e9314b97096024d78cd531d3bde6
Cihat Solak <cihatsolak@hotmail.com>
2023-02-27 20:41:24 +0300

    Add header exchange consumer-publisher

---
+++ ExchangeTypes/RabbitMQ.ExchangeTypes.Consumer/HeaderExchange.cs
@@ -0,0 +1,58 @@
+﻿namespace RabbitMQ.ExchangeTypes.Consumer
+{
+    internal static class HeaderExchange
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "header-exchange-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            channel.QueueBind(
+                queue: "header-exchange-queue",
+                exchange: "header-exchange",
+                routingKey: string.Empty,
+                arguments: new Dictionary<string, object>()
+                           {
+                               { "x-match", "all" }, //default: any geçerlidir
+                               { "x-private-key", "7f98e2b5-1071-4516-a6f7-70f186e8a85e" }
+                           }
+                );
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: "header-exchange-queue",
+                autoAck: false, //true: mesajların işlendiğinde otomatik sil. false: mesajları silmen için ben sana haber edeceğim.
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                string message = Encoding.UTF8.GetString(args.Body.Span);
+                Console.WriteLine("Message: {0}", message);
+
+                channel.BasicAck(
+                    deliveryTag: args.DeliveryTag,
+                    multiple: false
+                    );
+            };
+        }
+    }
+}
---
+++ ExchangeTypes/RabbitMQ.ExchangeTypes.Publisher/HeaderExchange.cs
@@ -0,0 +1,44 @@
+﻿namespace RabbitMQ.ExchangeTypes.Publisher
+{
+    internal static class HeaderExchange
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                exchange: "header-exchange",
+                type: ExchangeType.Headers,
+                durable: true,
+                autoDelete: false
+                );
+
+            IBasicProperties basicProperties = channel.CreateBasicProperties();
+            basicProperties.Headers = new Dictionary<string, object>()
+            {
+                { "x-private-key", "7f98e2b5-1071-4516-a6f7-70f186e8a85e" }
+            };
+
+            channel.BasicPublish(
+                exchange: "header-exchange",
+                routingKey: string.Empty,
+                body: Encoding.UTF8.GetBytes("Hello Header Exchange!"),
+                basicProperties: basicProperties
+                );
+
+
+            Console.ReadKey();
+        }
+    }
+}
