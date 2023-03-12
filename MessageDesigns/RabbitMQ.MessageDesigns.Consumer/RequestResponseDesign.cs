5a3a2dfd8181f6987d4a56322151f17e3047110b
Cihat Solak <cihatsolak@hotmail.com>
2023-03-12 17:21:38 +0300

    Add request response message design

---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Consumer/RequestResponseDesign.cs
@@ -0,0 +1,60 @@
+﻿namespace RabbitMQ.MessageDesigns.Consumer
+{
+    internal static class RequestResponseDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string requestQueueName = "request-response-queue";
+
+            channel.QueueDeclare(
+                queue: requestQueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: requestQueueName,
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                string message = Encoding.UTF8.GetString(args.Body.Span);
+                Console.WriteLine($"Incoming Message: {message}");
+
+                byte[] responseMessage = Encoding.UTF8.GetBytes($"Process completed. Message: {message}");
+
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.CorrelationId = args.BasicProperties.CorrelationId;
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: args.BasicProperties.ReplyTo,
+                    basicProperties: basicProperties,
+                    body: responseMessage
+                    );
+            };
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Publisher/RequestResponseDesign.cs
@@ -0,0 +1,67 @@
+﻿namespace RabbitMQ.MessageDesigns.Publisher
+{
+    internal static class RequestResponseDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string requestQueueName = "request-response-queue";
+
+            channel.QueueDeclare(
+                queue: requestQueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            string replyQueueName = channel.QueueDeclare().QueueName;
+            string correlationId = Guid.NewGuid().ToString();
+
+            IBasicProperties basicProperties = channel.CreateBasicProperties();
+            basicProperties.CorrelationId = correlationId;
+            basicProperties.ReplyTo = replyQueueName;
+
+            for (int index = 0; index < 100; index++)
+            {
+                byte[] message = Encoding.UTF8.GetBytes($"Hello {index}");
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: requestQueueName,
+                    basicProperties: basicProperties,
+                    body: message
+                    );
+            }
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: replyQueueName,
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                if (args.BasicProperties.CorrelationId == correlationId)
+                {
+                    Console.WriteLine($"Response: {Encoding.UTF8.GetString(args.Body.Span)}");
+                }
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
