a107753b8559adfbae4e93b0292eb0a257565068
Cihat Solak <cihatsolak@hotmail.com>
2023-03-12 12:37:06 +0300

    Add publisher subscriber message  design

---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Consumer/PubSubDesign.cs
@@ -0,0 +1,51 @@
+﻿namespace RabbitMQ.MessageDesigns.Consumer
+{
+    internal static class PubSubDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string exchangeName = "pub-sub-exchange";
+            string randomQueueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(
+                queue: randomQueueName,
+                exchange: exchangeName,
+                routingKey: string.Empty
+                );
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicQos(
+                prefetchCount: 1,
+                prefetchSize: 0,
+                global: false
+                );
+
+            channel.BasicConsume(
+                queue: randomQueueName,
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                Console.WriteLine($"Message: {Encoding.UTF8.GetString(args.Body.Span)}");
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Publisher/PubSubDesign.cs
@@ -0,0 +1,38 @@
+﻿namespace RabbitMQ.MessageDesigns.Publisher
+{
+    internal static class PubSubDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string exchangeName = "pub-sub-exchange";
+
+            channel.ExchangeDeclare(
+                exchange: exchangeName,
+                type: ExchangeType.Fanout
+                );
+
+            byte[] message = Encoding.UTF8.GetBytes("Hello!");
+
+            channel.BasicPublish(
+                exchange: exchangeName,
+                routingKey: string.Empty,
+                body: message
+                );
+
+            Console.ReadKey();
+        }
+    }
+}
