1c3fe0fee8c3017ec33fc9850132c3c19c421a3d
Cihat Solak <cihatsolak@hotmail.com>
2023-03-12 12:13:57 +0300

    Add point to point message design

---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Consumer/PointToPointDesign.cs
@@ -0,0 +1,38 @@
+﻿namespace RabbitMQ.MessageDesigns.Consumer
+{
+    internal static class PointToPointDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "p2p-queue";
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                Console.WriteLine($"Message: {Encoding.UTF8.GetString(args.Body.Span)}");
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Publisher/PointToPointDesign.cs
@@ -0,0 +1,40 @@
+﻿namespace RabbitMQ.MessageDesigns.Publisher
+{
+    internal static class PointToPointDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "p2p-queue";
+
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            byte[] message = Encoding.UTF8.GetBytes("Hello");
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queueName,
+                body: message
+                );
+
+            Console.ReadKey();
+        }
+    }
+}
