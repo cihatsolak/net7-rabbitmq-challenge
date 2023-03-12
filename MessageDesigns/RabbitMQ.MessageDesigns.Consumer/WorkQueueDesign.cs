3629257fd654f8bfe643e5f5c626d2ba5c921e99
Cihat Solak <cihatsolak@hotmail.com>
2023-03-12 16:49:05 +0300

    Add work queue message design

---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Consumer/WorkQueueDesign.cs
@@ -0,0 +1,51 @@
+﻿namespace RabbitMQ.MessageDesigns.Consumer
+{
+    internal static class WorkQueueDesign
+    {
+        internal static void Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "work-queue";
+
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false
+                );
+
+            EventingBasicConsumer eventingBasicConsumer = new(channel);
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: eventingBasicConsumer
+                );
+
+            channel.BasicQos( //ölçeklendirme
+                prefetchCount: 1, //her bir consumer 1 tane mesaj işleyecek
+                prefetchSize: 0, //sınırsız mesaj alabilir
+                global: false
+                );
+
+            eventingBasicConsumer.Received += (sender, args) =>
+            {
+                Console.WriteLine($"Message: {Encoding.UTF8.GetString(args.Body.Span)}");
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessageDesigns/RabbitMQ.MessageDesigns.Publisher/WorkQueueDesign.cs
@@ -0,0 +1,46 @@
+﻿namespace RabbitMQ.MessageDesigns.Publisher
+{
+    internal static class WorkQueueDesign
+    {
+        internal static async Task Run()
+        {
+            //Bağlantı oluşturma
+            IConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Port = 5672,
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            //Bağlantı aktifleştirme ve kanal açma
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            string queueName = "work-queue";
+
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false
+                );
+
+
+            for (int index = 0; index < 150; index++)
+            {
+                await Task.Delay(400);
+
+                byte[] message = Encoding.UTF8.GetBytes($"Hello {index}");
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: queueName,
+                    body: message
+                    );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
